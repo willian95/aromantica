395dc50229c4f5fe787cb1b3e4b9d728c921ba3f
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-06-24 13:59:17 -0400

    cambios

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class LoginController extends Controller
 {
