4fc325f41b55e0dfa3a51972f9016f8a04b7353b
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-06-24 13:41:47 -0400

    email registro

--- resources/views/emails/register.blade.php
+++ resources/views/emails/register.blade.php
@@ -1,1 +1,2 @@
-Hey
+Hola $user['name'], haz el click en el siguiente link para validar tu correo:
+<a href="{{ url('/email/validate/'.$hash) }}">Validar</a>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::get('/', function () {
 });
 
 Route::post("/register", "RegisterController@register");
+Route::get("/email/check/{hash}", "RegisterController@validate");
 Route::post("/login", "LoginController@login");
 Route::get("/logout", "LoginController@logout");
 
@@ -35,17 +36,3 @@ Route::delete("/cart/delete", "CartController@delete");
 
 Route::get("/shopping/index", "ShoppingController@index");
 Route::get("/shopping/fetch/{page}", "ShoppingController@fetch");
-
-Route::get("/test/mail", function(){
-
-    $to_name = "Willian";
-    $to_email = "rodriguezwillian95@gmail.com";
-
-    \Mail::send("emails.register", [], function($message) use ($to_name, $to_email) {
-
-        $message->to($to_email, $to_name)->subject("¡Solo falta un paso tu registro!");
-        $message->from("ventas@aromantica.co","Aromantica");
-
-    });
-
-});
