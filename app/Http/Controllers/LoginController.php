bb55b8febf5c8637137772d949a28bbf5db8c91a
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-06-24 13:58:11 -0400

    cambios

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,11 +13,20 @@ class LoginController extends Controller
 
         try{
 
+            $user = User::where("email", $request->email)->first();
+            if($user){
+
+                if($user->email_verified_at == null){
+                    return response()->json(["success" => false, "msg" => "Aún no has autenticado tu correo"]);    
+                }
+
+            }else{
+                return response()->json(["success" => false, "msg" => "Usuario no encontrado"]);
+            }
+
             if (Auth::attempt(['email' => $request->email, 'password' => $request->password], true)) {
                 return response()->json(["success" => true, "msg" => "Usuario autenticado", "role_id" => Auth::user()->role_id]);
             }
-
-            return response()->json(["success" => false, "msg" => "Usuario no encontrado"]);
 
         }catch(\Exception $e){
 
