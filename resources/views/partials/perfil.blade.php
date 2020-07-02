01d7d3e5427e84dbbda2af77681d1f1c43344553
patriciax <yively.pa@gmail.com>
2020-07-01 23:51:54 -0400

    add perfil

---
+++ resources/views/partials/perfil.blade.php
@@ -0,0 +1,48 @@
+<section class="container">
+    <div class="car">
+        <div class="card-body">
+            <div class="title__general title__general2 fadeInUp wow animated title__general_flex">
+                <p class="m-0">Perfil</p>
+
+                <div class=" form-group  text-center">
+                    <button class="btn btn-primary btn-custom "">Editar</button>
+             
+               </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="email">Nombre y apellido</label>
+                        <input placeholder="pedro perez" type="email" autocomplete="off" class="form-control"
+                            id="email" aria-describedby="emailHelp">
+                        <i class="fa fa-user icon_form"></i>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Correo electrónico</label>
+                        <input placeholder="pedroperez@gmail.com" type="email" autocomplete="off"
+                            class="form-control" id="email" aria-describedby="emailHelp">
+                        <i class="fa fa-envelope icon_form"></i>
+                    </div>
+
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="password">Dirección</label>
+
+                        <input placeholder="Dirección" type="password" class="form-control  " id="password">
+                        <i class="fa fa-globe icon_form"></i>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Telefono</label>
+
+                        <input placeholder="+1234567" type="password" class="form-control  " id="password">
+                        <i class="fa fa-phone icon_form"></i>
+                    </div>
+                </div>
+            </div>
+
+            
+    </div>
+  </div>
+
+</section>
