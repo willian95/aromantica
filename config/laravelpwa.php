4db816d71fd7b4e1f63d75aa6e436a50c8a0f2c8
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-08-24 18:29:21 -0400

    cambios

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -7,7 +7,7 @@ return [
         'short_name' => 'PWA',
         'start_url' => '/',
         'background_color' => '#ffffff',
-        'theme_color' => '#000000',
+        'theme_color' => '#352961',
         'display' => 'standalone',
         'orientation'=> 'any',
         'status_bar'=> 'black',
