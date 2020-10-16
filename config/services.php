ab85a569f391eff6411f89a5567d0a6ed3365e74
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-10-16 14:34:04 -0400

    cambios

--- config/services.php
+++ config/services.php
@@ -27,7 +27,7 @@ return [
     'google' => [
         'client_id' => '270786535137-tc6hofsn5ijmicm7dkdfihni8ta8j20g.apps.googleusercontent.com',
         'client_secret' => 'nkdylfOeY0dOCerNoDn3fJ3p',
-        'redirect' => 'https://www.aromantica.co/google/login/callback'],
+        'redirect' => url('/google/login/callback')/*'https://www.aromantica.co/google/login/callback'*/],
 
     'google2' => [
         'client_id' => '270786535137-tc6hofsn5ijmicm7dkdfihni8ta8j20g.apps.googleusercontent.com',
