29bf47f2dc2f4bfe85f6e2b1a192e25eb12af6c3
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-11-20 14:44:00 -0400

    top perfumes discount

--- resources/views/partials/top.blade.php
+++ resources/views/partials/top.blade.php
@@ -49,8 +49,14 @@
                                              </div>
                                          </div>
                                          <div class="main-top__price">
-                                             <p><span> $
-                                                     {{ number_format($topProduct->productTypeSize->price, 0, ",", ".") }}</span>
+                                             <p>
+                                                @if($topProduuct->productTypeSize->discountPercentage == 0)
+                                                    <span> ${{ number_format($topProduct->productTypeSize->price, 0, ",", ".") }}</span>
+                                                @else
+                                                    <span> ${{ number_format($topProduct->productTypeSize->price - (($topProduuct->productTypeSize->discountPercentage/100)*$topProduuct->productTypeSize->price), 0, ",", ".") }}</span>
+                                                    <strike>${{ number_format($topProduct->productTypeSize->price, 0, ",", ".") }}</strike>
+                                                @endif
+                                                   
                                              </p>
                                          </div>
 
@@ -79,47 +85,7 @@
 
                          </div>
                          @endforeach
-                         <!--<div>
-                <div class="main-top__container">
-                  <div class="main-top__item">
-                    <div class="main-top__text">
-                      <div class="main-top__title">
-                        <p>CH MEN PRIVÉ </p>
-                      </div>
-
-                      <div class="main-top__description">
-                        <p>CH Men Privé es una fragancia refinada y un tributo a la masculinidad extremadamente
-                          cautivador. Una firma sensual, al mismo tiempo rica, con textura y misteriosa. CH Men Privé es
-                          cálida y lujosa, con notas de whisky y de cuero, provocativa, moderna y rica.</p>
-                      </div>
-                      <div class="main-top__price">
-                        <p><span> $85.000</span> </p>
-
-                      </div>
-
-                      <div class="barra">
-                        <p> Vendidos:<span> 12</span></p>
-                        <p>Disponible: <span>28</span></p>
-                      </div>
-                      <div class="progress">
-                        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25"
-                          aria-valuemin="0" aria-valuemax="10"></div>
-                      </div>
-                      <div class="float-left main-top__btn">
-                        <a class="btn-custom" href="#">
-                          Ordene ya >
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="main-top__item">
-                    <div class="main-top__img">
-                      <img src="assets/img/productos/perfume2.png">
-                    </div>
-                  </div>
-                </div>
-
-              </div>-->
+                         
 
                      </div>
                      <!---cards detalle---->
